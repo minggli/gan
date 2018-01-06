abc932c77db0c79dab0d419e9cddc0dced666ade
Ming Li <ming.george.li@gmail.com>
2018-01-06 01:04:39 +0000

    get data

--- run.py
+++ run.py
@@ -1,5 +1,10 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+# use framework pointer to download data.
+from tensorflow.examples.tutorials.mnist import input_data
+mnist = input_data.read_data_sets("mnist_data/", one_hot=True)
+
+
 if __name__ == '__main__':
     print('Hello world.')
