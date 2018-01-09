f2b63337aa86212203d7ebba4d42d663d394ad1a
Ming Li <ming.george.li@gmail.com>
2018-01-09 19:01:40 +0000

    generative

--- run.py
+++ run.py
@@ -11,11 +11,18 @@ mnist = input_data.read_data_sets("mnist_data/", one_hot=True)
 
 print(mnist.train.next_batch(50)[0].shape)
 
-# generative
-g = BasicCNN([None, 100], 10)
-# g_dense_1 = g.add_dense_layer(x, [[None, 100], [100], [1 * 1 * 4 * 4]])
-# g_conv_1 = g.add_conv_layer(g_dense_1, [[100, 128], [128]], func='relu', bn=True)
+# generative network
+g = BasicCNN(shape=(1, 1, 100), num_classes=10)
+gx, gy_ = g.x, g.y_
+g_conv_1 = g.add_conv_layer(gx, [[1, 1, 100, 1024], [1024]], func='lrelu')
+g_conv_2 = g.add_conv_layer(g_conv_1, [[3, 3, 1024, 512], [512]], func='lrelu')
+g_conv_3 = g.add_conv_layer(g_conv_2, [[3, 3, 512, 256], [256]], func='lrelu')
+g_conv_4 = g.add_conv_layer(g_conv_3, [[3, 3, 256, 128], [128]], func='lrelu')
+go = g.add_conv_layer(g_conv_4, [[3, 3, 128, 1]], [1], func='tanh', bn=False)
+print(go.shape)
 
+# discriminative network
+d = BasicCNN(shape=(784, ), num_classes=10)
 
 if __name__ == '__main__':
     print('Hello world.')
