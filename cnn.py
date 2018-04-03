6e8ab12504f20ddaebe661075d301db7becf48f7
Ming Li <ming.george.li@gmail.com>
2018-04-03 22:36:32 +0100

    use get_variable and variable scope to handle reused weights and bias

--- cnn.py
+++ cnn.py
@@ -24,13 +24,17 @@ class _BaseCNN(object):
 
     @staticmethod
     def _weight_variable(shape):
-        initial = tf.truncated_normal(shape, stddev=0.1)
-        return tf.Variable(initial, name='weight')
+        init = tf.truncated_normal_initializer(stddev=0.1)
+        return tf.get_variable('weight',
+                               shape=shape,
+                               initializer=init)
 
     @staticmethod
     def _bias_variable(shape):
-        initial = tf.constant(0.1, shape=shape)
-        return tf.Variable(initial, name='bias')
+        init = tf.constant_initializer(0.1)
+        return tf.get_variable('bias',
+                               shape=shape,
+                               initializer=init)
 
     @staticmethod
     def _conv2d(x, W):
@@ -66,7 +70,8 @@ class _BaseCNN(object):
 
     @staticmethod
     def _nonlinearity(activation='relu'):
-        assert activation in ['relu', 'sigmoid', 'tanh', 'lrelu'], "unspecified activation function."
+        assert activation in ['relu', 'sigmoid', 'tanh', 'lrelu'], \
+                              "unspecified activation function."
         if activation == 'sigmoid':
             return tf.nn.sigmoid
         elif activation == 'relu':
@@ -125,50 +130,58 @@ class BasicCNN(_BaseCNN):
     def __init__(self, shape, num_classes, keep_prob=.5):
         super(BasicCNN, self).__init__(shape, num_classes, keep_prob)
 
-    def add_conv_layer(self, input_layer, hyperparams, func='relu', bn=True):
+    def add_conv_layer(self, input_layer, name, hyperparams, func='lrelu',
+                       bn=True):
         """Convolution Layer with hyperparamters and activation and batch
         normalization after nonlinearity as opposed to before nonlinearity as
         cited in Ioffe and Szegedy 2015."""
-        W = self._weight_variable(shape=hyperparams[0])
-        b = self._bias_variable(shape=hyperparams[1])
-        if bn:
-            return self._batch_normalize(
-                   self._nonlinearity(func)(self._conv2d(input_layer, W) + b))
-        elif not bn:
-            return self._nonlinearity(func)(self._conv2d(input_layer, W) + b)
+        with tf.variable_scope(name):
+            W = self._weight_variable(shape=hyperparams[0])
+            b = self._bias_variable(shape=hyperparams[1])
+            if bn:
+                return self._batch_normalize(self._nonlinearity(func)(
+                                             self._conv2d(input_layer, W) + b))
+            elif not bn:
+                return self._nonlinearity(func)(
+                                             self._conv2d(input_layer, W) + b)
 
     def add_pooling_layer(self,
                           input_layer,
+                          name,
                           kernel_size=[1, 2, 2, 1],
                           mode='max'):
         """max pooling layer to reduce overfitting"""
-        if mode == 'max':
-            return self._max_pool(input_layer, kernel_size)
-        elif mode == 'average':
-            return self._average_pool(input_layer, kernel_size)
+        with tf.variable_scope(name):
+            if mode == 'max':
+                return self._max_pool(input_layer, kernel_size)
+            elif mode == 'average':
+                return self._average_pool(input_layer, kernel_size)
 
-    def add_dense_layer(self, input_layer, hyperparams, func='relu', bn=True):
+    def add_dense_layer(self, input_layer, name, hyperparams, func='lrelu',
+                        bn=True):
         """Densely Connected Layer with hyperparamters and activation. Batch
         normalization inserted after nonlinearity as opposed to before as
         cited in Ioffe and Szegedy 2015."""
-        W = self._weight_variable(shape=hyperparams[0])
-        b = self._bias_variable(shape=hyperparams[1])
-        x_ravel = tf.reshape(input_layer, shape=[-1, hyperparams[0][0]])
-        if bn:
-            return self._batch_normalize(
-                   self._nonlinearity(func)(tf.matmul(x_ravel, W) + b))
-        elif not bn:
-            return self._nonlinearity(func)(tf.matmul(x_ravel, W) + b)
+        with tf.variable_scope(name):
+            W = self._weight_variable(shape=hyperparams[0])
+            b = self._bias_variable(shape=hyperparams[1])
+            x_ravel = tf.reshape(input_layer, shape=[-1, hyperparams[0][0]])
+            if bn:
+                return self._batch_normalize(
+                       self._nonlinearity(func)(tf.matmul(x_ravel, W) + b))
+            elif not bn:
+                return self._nonlinearity(func)(tf.matmul(x_ravel, W) + b)
 
-    def add_drop_out_layer(self, input_layer):
+    def add_drop_out_layer(self, input_layer, name):
         """drop out layer to reduce overfitting"""
-        return tf.nn.dropout(input_layer, self.keep_prob)
+        with tf.variable_scope(name):
+            return tf.nn.dropout(input_layer, self.keep_prob)
 
-    def add_read_out_layer(self, input_layer):
+    def add_read_out_layer(self, input_layer, name):
         """read out layer with output shape of [batch_size, num_classes]
         in order to feed into softmax"""
-        input_layer_m = int(input_layer.get_shape()[1])
-        W = self._weight_variable(shape=[input_layer_m, self._n_class])
-        b = self._bias_variable(shape=[self._n_class])
-
-        return tf.matmul(input_layer, W) + b
+        with tf.variable_scope(name):
+            input_layer_m = int(input_layer.get_shape()[1])
+            W = self._weight_variable(shape=[input_layer_m, self._n_class])
+            b = self._bias_variable(shape=[self._n_class])
+            return tf.matmul(input_layer, W) + b
