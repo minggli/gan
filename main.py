8e7f61a6d334a7e51da2f95eaacc7a0a62a392c9
minggli <ming.george.li@gmail.com>
2018-04-05 21:51:51 +0100

    tranposed convolution

--- main.py
+++ main.py
@@ -1,82 +1,188 @@
 #! /usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""
+Simple Deep Convolutional Generative Adversial Networks (DCGANs) with MNIST
+data.
+"""
 
 import tensorflow as tf
-# use framework pointer to download data.
-from tensorflow.examples.tutorials.mnist import input_data
 
-from cnn import BasicCNN
+from functools import wraps
+from models.official.mnist.dataset import train
+
 
 BATCH_SIZE = 50
+tf.set_random_seed(0)
 
-mnist = input_data.read_data_sets("mnist_data/", one_hot=True)
+# global is_train flag for both generative and discriminative models.
+is_train = tf.placeholder_with_default(input=False, shape=[], name='is_train')
 
-print(mnist.train.next_batch(BATCH_SIZE)[0].shape)
 
+def weight_variable(shape):
+    init = tf.truncated_normal_initializer(stddev=0.1)
+    return tf.get_variable('weight', shape=shape, initializer=init)
+
+
+def bias_variable(shape):
+    init = tf.constant_initializer(0.1)
+    return tf.get_variable('bias', shape=shape, initializer=init)
+
+
+def batch_norm(is_training):
+    def decorator(func):
+        """batch normalization"""
+        @wraps(func)
+        def wrapper(arg):
+            return tf.layers.batch_normalization(inputs=func(arg),
+                                                 axis=-1,
+                                                 momentum=0.99,
+                                                 epsilon=0.001,
+                                                 center=True,
+                                                 scale=True,
+                                                 training=is_training,
+                                                 reuse=None)
+        return wrapper
+    return decorator
+
+
+@batch_norm(is_train)
+def lrelu(tensor, alpha=.2):
+    """Leaky Rectified Linear Unit, alleviating gradient vanishing."""
+    return tf.maximum(alpha * tensor, tensor)
+
+
+mnist_train = train("./mnist_data/").shuffle(1000).repeat().batch(BATCH_SIZE)
+
+# construct generative network using transposed convolution layers
+# (also known as deconvolution) generate images from white noise signal.
 with tf.variable_scope('generator', reuse=tf.AUTO_REUSE):
     # generative network
-    g = BasicCNN(shape=(1, 1, 100), num_classes=10)
-    g_x, g_y_, g_is_train = g.x, g.y_, g.is_train
-    with tf.variable_scope('conv_1'):
-        W = g._weight_variable(shape=[1, 1, 100, 1024])
-        b = g._bias_variable(shape=[1024])
-        g_conv_1 = g._batch_normalize(g._nonlinearity('lrelu')(
-                                tf.nn.conv2d(input=g_x,
-                                             filter=W,
-                                             strides=[1, 1, 1, 1],
-                                             padding='VALID') + b))
-    g_conv_2 = g.add_conv_layer(g_conv_1, 'conv_2',
-                                [[4, 4, 1024, 512], [512]])
-    g_conv_3 = g.add_conv_layer(g_conv_2, 'conv_3',
-                                [[4, 4, 512, 256], [256]])
-    g_conv_4 = g.add_conv_layer(g_conv_3, 'conv_4',
-                                [[4, 4, 256, 128], [128]])
-    g_logits = g.add_conv_layer(g_conv_4, 'logits',
-                                [[4, 4, 128, 1], [1]], func=None, bn=False)
+    g_x = tf.random_normal([-1, 1, 1, 100], name='gaussian')
+    with tf.variable_scope('deconv_1'):
+        W = weight_variable([1, 1, 1024, 100])
+        b = bias_variable([1024])
+        g_conv_1 = lrelu(tf.nn.conv2d_transpose(g_x,
+                                                filter=W,
+                                                output_shape=[-1, 4, 4, 1024],
+                                                strides=[1, 1, 1, 1],
+                                                padding='VALID') + b)
+    with tf.variable_scope('deconv_2'):
+        W = weight_variable([4, 4, 512, 1024])
+        b = bias_variable([512])
+        g_conv_2 = lrelu(tf.nn.conv2d_transpose(g_conv_1,
+                                                filter=W,
+                                                output_shape=[-1, 8, 8, 512],
+                                                strides=[1, 2, 2, 1],
+                                                padding='SAME') + b)
+    with tf.variable_scope('deconv_3'):
+        W = weight_variable([4, 4, 256, 512])
+        b = bias_variable([256])
+        g_conv_3 = lrelu(tf.nn.conv2d_transpose(g_conv_2,
+                                                filter=W,
+                                                output_shape=[-1, 16, 16, 256],
+                                                strides=[1, 2, 2, 1],
+                                                padding='SAME') + b)
+    with tf.variable_scope('deconv_4'):
+        W = weight_variable([4, 4, 128, 256])
+        b = bias_variable([128])
+        g_conv_4 = lrelu(tf.nn.conv2d_transpose(g_conv_3,
+                                                filter=W,
+                                                output_shape=[-1, 32, 32, 128],
+                                                strides=[1, 2, 2, 1],
+                                                padding='SAME') + b)
+    with tf.variable_scope('logits'):
+        W = weight_variable([4, 4, 1, 128])
+        b = bias_variable([1])
+        g_logits = tf.nn.conv2d_transpose(g_conv_4,
+                                          filter=W,
+                                          output_shape=[-1, 64, 64, 1],
+                                          strides=[1, 2, 2, 1],
+                                          padding='SAME') + b
     g_o = tf.nn.tanh(g_logits)
 
+# there is only one single discriminative network with variables reused
+with tf.variable_scope('discriminator', reuse=tf.AUTO_REUSE):
+    # discriminative network for fake (generated) images
+    with tf.variable_scope('conv_1'):
+        W = weight_variable([4, 4, 1, 128])
+        b = bias_variable([128])
+        d_fake_conv_1 = lrelu(tf.nn.conv2d(input=g_o,
+                                           filter=W,
+                                           strides=[1, 2, 2, 1],
+                                           padding='SAME') + b)
+    with tf.variable_scope('conv_2'):
+        W = weight_variable([4, 4, 128, 256])
+        b = bias_variable([256])
+        d_fake_conv_2 = lrelu(tf.nn.conv2d(input=d_fake_conv_1,
+                                           filter=W,
+                                           strides=[1, 2, 2, 1],
+                                           padding='SAME') + b)
+    with tf.variable_scope('conv_3'):
+        W = weight_variable([4, 4, 256, 512])
+        b = bias_variable([512])
+        d_fake_conv_3 = lrelu(tf.nn.conv2d(input=d_fake_conv_2,
+                                           filter=W,
+                                           strides=[1, 2, 2, 1],
+                                           padding='SAME') + b)
+    with tf.variable_scope('conv_4'):
+        W = weight_variable([4, 4, 512, 1024])
+        b = bias_variable([1024])
+        d_fake_conv_4 = lrelu(tf.nn.conv2d(input=d_fake_conv_3,
+                                           filter=W,
+                                           strides=[1, 2, 2, 1],
+                                           padding='SAME') + b)
+    with tf.variable_scope('logits'):
+        W = weight_variable([4, 4, 1024, 1])
+        b = bias_variable([1])
+        d_fake_logits = tf.nn.conv2d(input=d_fake_conv_4,
+                                     filter=W,
+                                     strides=[1, 1, 1, 1],
+                                     padding='VALID') + b
+    d_fake_o = tf.nn.sigmoid(d_fake_logits)
+
+# there is only one single discriminative network with variables reused
 with tf.variable_scope('discriminator', reuse=tf.AUTO_REUSE):
     # discriminative network for real images
-    d_real = BasicCNN(shape=(64, 64, 1), num_classes=10)
-    d_real_x, d_real_y_, d_real_is_train = d_real.x, d_real.y_, d_real.is_train
-    d_real_conv_1 = d_real.add_conv_layer(d_real_x, 'conv_1',
-                                          [[4, 4, 1, 128], [128]])
-    d_real_conv_2 = d_real.add_conv_layer(d_real_conv_1, 'conv_2',
-                                          [[4, 4, 128, 256], [256]])
-    d_real_conv_3 = d_real.add_conv_layer(d_real_conv_2, 'conv_3',
-                                          [[4, 4, 256, 512], [512]])
-    d_real_conv_4 = d_real.add_conv_layer(d_real_conv_3, 'conv_4',
-                                          [[4, 4, 512, 1024], [1024]])
+    d_real_x = tf.placeholder(dtype=tf.float32, shape=[None, 64, 64, 1],
+                              name='feature')
+
+    with tf.variable_scope('conv_1'):
+        W = weight_variable([4, 4, 1, 128])
+        b = bias_variable([128])
+        d_real_conv_1 = lrelu(tf.nn.conv2d(input=d_real_x,
+                                           filter=W,
+                                           strides=[1, 2, 2, 1],
+                                           padding='SAME') + b)
+    with tf.variable_scope('conv_2'):
+        W = weight_variable([4, 4, 128, 256])
+        b = bias_variable([256])
+        d_real_conv_2 = lrelu(tf.nn.conv2d(input=d_real_conv_1,
+                                           filter=W,
+                                           strides=[1, 2, 2, 1],
+                                           padding='SAME') + b)
+    with tf.variable_scope('conv_3'):
+        W = weight_variable([4, 4, 256, 512])
+        b = bias_variable([512])
+        d_real_conv_3 = lrelu(tf.nn.conv2d(input=d_real_conv_2,
+                                           filter=W,
+                                           strides=[1, 2, 2, 1],
+                                           padding='SAME') + b)
+    with tf.variable_scope('conv_4'):
+        W = weight_variable([4, 4, 512, 1024])
+        b = bias_variable([1024])
+        d_real_conv_4 = lrelu(tf.nn.conv2d(input=d_real_conv_3,
+                                           filter=W,
+                                           strides=[1, 2, 2, 1],
+                                           padding='SAME') + b)
     with tf.variable_scope('logits'):
-        W = g._weight_variable(shape=[4, 4, 1024, 1])
-        b = g._bias_variable(shape=[1])
+        W = weight_variable([4, 4, 1024, 1])
+        b = bias_variable([1])
         d_real_logits = tf.nn.conv2d(input=d_real_conv_4,
                                      filter=W,
                                      strides=[1, 1, 1, 1],
                                      padding='VALID') + b
     d_real_o = tf.nn.sigmoid(d_real_logits)
 
-# there is only one single discriminative network with variables reused
-with tf.variable_scope('discriminator', reuse=tf.AUTO_REUSE):
-    # discriminative network for fake (generated) images
-    d_fake = BasicCNN(shape=(64, 64, 1), num_classes=10)
-    d_fake_x, d_fake_y_, d_fake_is_train = d_fake.x, d_fake.y_, d_fake.is_train
-    d_fake_conv_1 = d_fake.add_conv_layer(d_fake_x, 'conv_1',
-                                          [[4, 4, 1, 128], [128]])
-    d_fake_conv_2 = d_fake.add_conv_layer(d_fake_conv_1, 'conv_2',
-                                          [[4, 4, 128, 256], [256]])
-    d_fake_conv_3 = d_fake.add_conv_layer(d_fake_conv_2, 'conv_3',
-                                          [[4, 4, 256, 512], [512]])
-    d_fake_conv_4 = d_fake.add_conv_layer(d_fake_conv_3, 'conv_4',
-                                          [[4, 4, 512, 1024], [1024]])
-    with tf.variable_scope('logits'):
-        W = g._weight_variable(shape=[4, 4, 1024, 1])
-        b = g._bias_variable(shape=[1])
-        d_fake_logits = tf.nn.conv2d(input=d_fake_conv_4,
-                                     filter=W,
-                                     strides=[1, 1, 1, 1],
-                                     padding='VALID') + b
-    d_fake_o = tf.nn.sigmoid(d_fake_logits)
 
 # cost functions for D(x) and G(z) respectively
 d_real_xentropy = tf.nn.sigmoid_cross_entropy_with_logits(
@@ -87,13 +193,12 @@ d_fake_xentropy = tf.nn.sigmoid_cross_entropy_with_logits(
                                     labels=tf.zeros([BATCH_SIZE, 1, 1, 1]))
 d_loss = tf.reduce_mean(d_real_xentropy) + tf.reduce_mean(d_fake_xentropy)
 g_xentropy = tf.nn.sigmoid_cross_entropy_with_logits(
-                                    logits=g_logits,
+                                    logits=d_fake_logits,
                                     labels=tf.ones([BATCH_SIZE, 1, 1, 1]))
 g_loss = tf.reduce_mean(g_xentropy)
 
-for var in tf.global_variables():
-    print(var.name)
+for var in tf.global_variables("generator"):
+    print(var)
 
-for i in [d_real_conv_1, d_real_conv_2, d_real_conv_3, d_real_conv_4,
-          d_real_logits]:
-    print(i.shape)
+for var in tf.global_variables("discriminator"):
+    print(var)
