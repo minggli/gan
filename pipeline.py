a5ac77c2cd724babb469fca752f84f769c54cb10
minggli <ming.george.li@gmail.com>
2018-04-06 20:34:08 +0100

    wip pip

--- pipeline.py
+++ pipeline.py
@@ -1,14 +1,25 @@
 import tensorflow as tf
 
-from models.official.mnist.dataset import train
+from models.official.mnist.dataset import train, test
 from config import BATCH_SIZE
-
-mnist_train = train("./mnist_data/").shuffle(1000).repeat().batch(BATCH_SIZE)
-iterator = mnist_train.make_one_shot_iterator()
-
-sample_batch = iterator.get_next()
-
 sess = tf.Session()
 
-images, labels = sess.run(sample_batch)
-print(images.shape)
+mnist_train = train("./mnist_data/")
+mnist_test = test("./mnist_data/")
+mnist = mnist_train.concatenate(mnist_test)
+mnist_batch = mnist.shuffle(1000).repeat().batch(BATCH_SIZE)
+mnist_batch_iter = mnist_batch.make_initializable_iterator()
+
+#
+# def fetch_batch(Iterator):
+#     with tf.Session() as sess:
+#         sess.run(tf.global_variables_initializer())
+#         data, labels = Iterator.get_next()
+#     return data.eval(session=tf.Session())
+#
+
+with sess:
+    init = mnist_batch_iter.initializer
+    sess.run(init)
+    data, labels = mnist_batch_iter
+    print(data.eval(session=sess))
