e69e74c820c4ec61c807ae8269f6e5afd432257a
Ming Li <ming.george.li@gmail.com>
2018-04-08 19:07:31 +0100

    standardize images

--- pipeline.py
+++ pipeline.py
@@ -11,6 +11,7 @@ mnist = train("./mnist_data/").concatenate(test("./mnist_data/"))
 mnist_images = mnist.map(lambda img, label: tf.reshape(img, [28, 28, 1]))
 mnist_images_resized = mnist_images.map(
                             lambda x: tf.image.resize_images(x, [64, 64]))
+mnist_images = mnist_images_resized.map(tf.image.per_image_standardization)
 mnist_batch = mnist_images_resized.shuffle(1000).repeat().batch(BATCH_SIZE)
 mnist_batch_iter = mnist_batch.make_one_shot_iterator()
 mnist_tensor = mnist_batch_iter.get_next()
