c5231e7238af941b4268ef1b511b289dcc86fa4b
minggli <ming.george.li@gmail.com>
2018-04-13 16:18:19 +0100

    move images to (-1, 1)

--- pipeline.py
+++ pipeline.py
@@ -11,6 +11,7 @@ EPOCH = NNConfig.EPOCH
 mnist = train("./mnist_data/")
 mnist_images = mnist.map(lambda img, label: tf.reshape(img, [28, 28, 1]))
 mnist_images = mnist_images.map(lambda x: tf.image.resize_images(x, [64, 64]))
+mnist_images = mnist_images.map(lambda x: (x - 0.5) / 0.5)
 
 mnist_batch = mnist_images.shuffle(1000).batch(BATCH_SIZE)
 mnist_batch_iter = mnist_batch.make_initializable_iterator()
