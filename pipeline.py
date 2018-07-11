4e0e9033b7529b524df54257888abf90a4c95fdf
Ming Li <ming.george.li@gmail.com>
2018-07-11 14:48:19 +0100

    unused parameter

--- pipeline.py
+++ pipeline.py
@@ -6,7 +6,6 @@ from config import NNConfig
 __all__ = ['feed', 'mnist_batch_iter']
 
 BATCH_SIZE = NNConfig.BATCH_SIZE
-EPOCH = NNConfig.EPOCH
 
 mnist = train("./mnist_data/")
 mnist_images = mnist.map(lambda img, label: tf.reshape(img, [28, 28, 1]))
