e783866d691ba3bf084242447ad8806f01d13e58
Ming Li <ming.george.li@gmail.com>
2018-07-12 18:40:23 +0100

    rename

--- graph.py
+++ graph.py
@@ -2,10 +2,10 @@
 # -*- encoding: utf-8 -*-
 import tensorflow as tf
 
+from pipeline import feed
+
 from graph import Generator, Discriminator, Loss
 from config import NNConfig, g_params, d_params
-from pipeline import feed, mnist_batch_iter
-from output import produce_grid, produce_gif
 
 BATCH_SIZE, EPOCH, LR, N_CRITIC = \
     NNConfig.BATCH_SIZE, NNConfig.EPOCH, NNConfig.ALPHA, NNConfig.N_CRITIC
@@ -40,49 +40,3 @@ with tf.control_dependencies(tf.get_collection(tf.GraphKeys.UPDATE_OPS)):
     g_train_step = tf.train.AdamOptimizer(LR, beta1=.5, beta2=.9).minimize(
                             g_loss,
                             var_list=tf.trainable_variables('Generator'))
-
-sess = tf.Session()
-init_op = tf.global_variables_initializer()
-
-sess.run(init_op)
-
-for var in tf.trainable_variables():
-    print(var)
-
-grids_through_epochs = list()
-constant = g.gaussian_noise(g_x)
-
-for epoch in range(1, EPOCH + 1):
-    try:
-        step = 0
-        sess.run(mnist_batch_iter.initializer)
-        while True:
-            try:
-                for i in range(N_CRITIC):
-                    step += 1
-                    _, d_loss_score = sess.run(
-                                        fetches=[d_train_step, d_loss],
-                                        feed_dict={g_x: g.gaussian_noise(g_x)})
-                    print("Epoch {0} of {1}, step {2} "
-                          "Discriminator log loss {3:.4f}".format(
-                            epoch, EPOCH, step, d_loss_score))
-
-                step += 1
-                _, g_loss_score = sess.run(
-                                        fetches=[g_train_step, g_loss],
-                                        feed_dict={g_x: g.gaussian_noise(g_x)})
-                print("Epoch {0} of {1}, step {2}"
-                      " Generator log loss {3:.4f}".format(
-                        epoch, EPOCH, step, g_loss_score))
-
-            except tf.errors.OutOfRangeError:
-                print("Epoch {0} has finished.".format(epoch))
-                break
-    except KeyboardInterrupt:
-        print("Ending Training during {0} epoch.".format(epoch))
-        break
-    test_images = sess.run(g_o, feed_dict={g_x: constant, is_train: False})
-    grids_through_epochs.append(
-        produce_grid(test_images, epoch, './results', save=True))
-
-produce_gif(grids_through_epochs, path='./results')
