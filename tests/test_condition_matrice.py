f2907aac2bc4403754b1e4f697d64669c5094e40
Ming Li <ming.george.li@gmail.com>
2018-09-03 16:31:51 +0100

    add tests

---
+++ tests/test_condition_matrice.py
@@ -0,0 +1,20 @@
+import numpy as np
+
+from helper import condition_matrice
+
+
+class TestConditionMatrice(object):
+
+    z = np.eye(10)[np.random.randint(0, 9, 100)]
+
+    def test_condition_generator(self):
+        y_gz, _ = condition_matrice(self.z)
+
+        assert np.allclose(y_gz[:, 0, 0, :], self.z)
+
+    def test_condition_discriminator(self):
+        _, y_dx = condition_matrice(self.z)
+        row, col = np.where(self.z == 1)
+
+        for r, c in zip(row, col):
+            assert (y_dx[r, 0, 0, c] == 1).all()
