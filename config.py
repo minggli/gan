3ac9e755c348b702af8c166c4307a06f437ef623
Ming Li <ming.george.li@gmail.com>
2018-09-04 16:38:27 +0100

    add appconfig

--- config.py
+++ config.py
@@ -12,18 +12,6 @@ tf.app.flags.DEFINE_integer('epochs', 20,
                             """Number of epochs to run.""")
 
 
-class BaseConfig(object):
-    pass
-
-
-class NNConfig(BaseConfig):
-    BATCH_SIZE = 64
-    EPOCH = FLAGS.epochs
-    ALPHA = 1e-4
-    N_CRITIC = 3
-    N_CLASS = 10
-
-
 d_params = [
     ('conv_1', [[4, 4, 1, 128], [128]], [1, 2, 2, 1], 'SAME'),
     ('conv_2', [[4, 4, 128, 256], [256]], [1, 2, 2, 1], 'SAME'),
@@ -44,3 +32,19 @@ g_params = [
     ('logits', [[4, 4, 1, 128], [1], [64, 64, 64, 1]],
      [1, 2, 2, 1], 'SAME')
 ]
+
+
+class BaseConfig(object):
+    pass
+
+
+class NNConfig(BaseConfig):
+    BATCH_SIZE = 64
+    EPOCH = FLAGS.epochs
+    ALPHA = 1e-4
+    N_CRITIC = 3
+    N_CLASS = 10
+
+
+class AppConfig(BaseConfig):
+    PORT = 8501
