5af126eb901aaa08c145904af43eed25bd74330e
Ming Li <ming.george.li@gmail.com>
2018-09-12 21:21:07 +0100

    update script

--- train_send.sh
+++ train_send.sh
@@ -3,7 +3,7 @@
 export SCRIPT_DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 cd $SCRIPT_DIR
 
-python -m gan.train
+sudo python3 -m gan.train
 aws s3 sync bin s3://mnist-gan-binaries/
 
 sudo shutdown -h now
