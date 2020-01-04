a047e5ca0aaa81b613698407c5c07f12b05385ef
Ming Li <ming.george.li@gmail.com>
2020-01-04 21:31:59 +0000

    add logging

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ app
 
 flask app serving external client calls.
 """
+import logging
 from random import randint
 
 from flask import Flask
@@ -13,9 +14,15 @@ from helper import _validate_integer
 from serving import grpc_generate
 from config import APP_CONFIG
 
+logger = logging.getLogger(__name__)
+
 application = Flask(__name__)
 application.config.update(APP_CONFIG)
 ns = Namespace('generate', description='Generate images.')
+api = Api(
+    title='Wasserstein GAN',
+    version='1.0',
+    description='Generate images using GANs')
 
 
 @ns.route('/', defaults={'digit': None})
@@ -25,8 +32,14 @@ class Generate(Resource):
     def get(self, digit):
         if _validate_integer(digit) is None:
             digit = randint(0, 9)
-        return grpc_generate(digit)
+        logger.info(f"request received to generate {digit}.")
+        img = grpc_generate(digit)
+        logger.info("image generated successfully.")
+        return img
 
+
+api.add_namespace(ns)
+api.init_app(application)
 
 # TODO Wasserstein loss doesn't discriminate real or fake image like original
 # GAN loss function, more work needed to reuse Critic/Discriminator
@@ -40,13 +53,6 @@ class Generate(Resource):
 #         prob = grpc_predict(f)
 #         return render_template('predict.html', result=prob)
 #     return render_template('predict.html')
-
-api = Api(
-    title='Wasserstein GAN',
-    version='1.0',
-    description='Generate images using GANs')
-api.add_namespace(ns)
-api.init_app(application)
 
 if __name__ == '__main__':
     application.run(host='0.0.0.0', port='8000', debug=True)
